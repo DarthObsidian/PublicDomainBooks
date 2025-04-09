c66003c6ab05a81c08775a9c67c13ddf2abc93e2
Matthew Meyers <DarthObsidian@users.noreply.github.com>
2025-04-08 23:01:49 -0600

    Started work on the Count of Monte-Cristo.

---
+++ CountOfMonteCristo/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ ! -d "./dump" ]; then
+	mkdir ./dump
+fi
+
+if [ ! $1 = s ]; then
+	lualatex --output-directory dump main.tex
+fi
+
+if [ $1 = a ] || [ $1 = s ]; then
+	lualatex --output-directory dump signature.tex
+fi
