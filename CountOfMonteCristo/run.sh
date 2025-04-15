a78bf5b404e53e41cadb71aa94a0bab15bce7292
Matthew Meyers <DarthObsidian@users.noreply.github.com>
2025-04-14 22:03:07 -0600

    Renamed so added other volumes is easy.

--- CountOfMonteCristo/run.sh
+++ CountOfMonteCristo/run.sh
@@ -4,10 +4,10 @@ if [ ! -d "./dump" ]; then
 	mkdir ./dump
 fi
 
-if [ ! $1 = s ]; then
-	lualatex --output-directory dump main.tex
+if [ $1 = vol1 ]; then
+	lualatex --output-directory dump vol1.tex
 fi
 
-if [ $1 = a ] || [ $1 = s ]; then
-	lualatex --output-directory dump signature.tex
+if [ $1 = vol1 ] && [ $2 = impose ]; then
+	lualatex --output-directory dump vol1-signature.tex
 fi
