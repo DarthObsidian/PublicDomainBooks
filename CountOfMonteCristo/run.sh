354657b40883bbfec58cb83d448fc7b2a0eba24a
Matthew Meyers <DarthObsidian@users.noreply.github.com>
2025-09-22 20:08:21 -0600

    Finished Count volume 2.

--- CountOfMonteCristo/run.sh
+++ CountOfMonteCristo/run.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 if [ ! -d "./dump" ]; then
-	mkdir ./dump
+    mkdir ./dump
 fi
 
 if [ $1 = vol1 ]; then
-	lualatex --output-directory dump vol1.tex
+    lualatex --output-directory dump vol1.tex
 fi
 
 if [ $1 = vol1 ] && [ $2 = impose ]; then
-	lualatex --output-directory dump vol1-signature.tex
+    lualatex --output-directory dump vol1-signature.tex
 fi
 
 if [ $1 = vol2 ]; then
-	lualatex --output-directory dump vol2.tex
+    lualatex --output-directory dump vol2.tex
 fi
+
+if [ $1 = vol2 ] && [ $2 = impose ]; then
+    lualatex --output-directory dump vol2-signature.tex
+fi
+
