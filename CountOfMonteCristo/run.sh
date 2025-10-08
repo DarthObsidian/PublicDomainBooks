2fa299e62ab63b671c8cf8dacd8bc1e4f6a9de25
Matthew Meyers <DarthObsidian@users.noreply.github.com>
2025-10-08 13:45:17 -0600

    Started vol 3. Fixed issues in vols 1 & 2.

--- CountOfMonteCristo/run.sh
+++ CountOfMonteCristo/run.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 
-if [ ! -d "./dump" ]; then
+if [[ ! -d "./dump" ]]; then
     mkdir ./dump
 fi
 
-if [ $1 = vol1 ]; then
+if [[ $1 = vol1 ]]; then
     lualatex --output-directory dump vol1.tex
 fi
 
-if [ $1 = vol1 ] && [ $2 = impose ]; then
+if [[ $1 = vol1 ]] && [[ $2 = impose ]]; then
     lualatex --output-directory dump vol1-signature.tex
 fi
 
-if [ $1 = vol2 ]; then
+if [[ $1 = vol2 ]]; then
     lualatex --output-directory dump vol2.tex
 fi
 
-if [ $1 = vol2 ] && [ $2 = impose ]; then
+if [[ $1 = vol2 ]] && [[ $2 = impose ]]; then
     lualatex --output-directory dump vol2-signature.tex
+fi
+
+if [[ $1 = vol3 ]]; then
+    lualatex --output-directory dump vol3.tex
+fi
+
+if [[ $1 = vol3 ]] && [[ $2 = impose ]]; then
+    lualatex --output-directory dump vol3-signature.tex
 fi
 
