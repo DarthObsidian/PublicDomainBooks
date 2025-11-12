9e9ad39c4c9aa958df7889e9bcb20c662133ee52
Matthew Meyers <DarthObsidian@users.noreply.github.com>
2025-11-12 10:47:21 -0700

    Started volume 5

--- CountOfMonteCristo/run.sh
+++ CountOfMonteCristo/run.sh
@@ -24,14 +24,22 @@ if [[ $1 = vol3 ]]; then
     lualatex --output-directory dump vol3.tex
 fi
 
-if [[ $1 = vol4 ]]; then
-    lualatex --output-directory dump vol4.tex
-fi
-
 if [[ $1 = vol3 ]] && [[ $2 = impose ]]; then
     lualatex --output-directory dump vol3-signature.tex
 fi
 
+if [[ $1 = vol4 ]]; then
+    lualatex --output-directory dump vol4.tex
+fi
+
 if [[ $1 = vol4 ]] && [[ $2 = impose ]]; then
     lualatex --output-directory dump vol4-signature.tex
+fi
+
+if [[ $1 = vol5 ]]; then
+    lualatex --output-directory dump vol5.tex
+fi
+
+if [[ $1 = vol5 ]] && [[ $2 = impose ]]; then
+    lualatex --output-directory dump vol5-signature.tex
 fi
